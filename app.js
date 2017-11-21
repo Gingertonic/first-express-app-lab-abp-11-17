f78f718bdb336666cbb7996a79154a799fbd5297
Beth Schofield <bethmschofield@gmail.com>
2017-11-21 23:23:28 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, resp){
-  resp.send ("Hello, World!")
+  resp.send (`Hello, World!`)
 }
 
 module.exports = app
